b6e3133becb9d2e5f1592b92dbda64d126864e37
balram0608 <balram0608@gmail.com>
2020-06-16 14:01:45 +0530

    ..

--- lib/pages/Welcome.dart
+++ lib/pages/Welcome.dart
@@ -67,7 +67,7 @@ class _WelcomePageState extends State<WelcomePage>
           gradient: LinearGradient(
               colors: [
                 Colors.white,
-                Colors.green[400],
+                Colors.green[200],
               ],
               begin: const FractionalOffset(0.0, 1.0),
               end: const FractionalOffset(0.0, 1.0),
