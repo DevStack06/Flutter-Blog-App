ab7376fa5be74a13863a36a32a5e9197825b6b3a
balram0608 <balram0608@gmail.com>
2020-06-17 22:50:57 +0530

    form validated

--- lib/pages/SignUp.dart
+++ lib/pages/SignUp.dart
@@ -8,6 +8,7 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final _globalKey = GlobalKey<FormState>();
   bool vis = false;
   @override
   Widget build(BuildContext context) {
@@ -24,45 +25,55 @@ class _SignUpPageState extends State<SignUpPage> {
             tileMode: TileMode.repeated,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Sign up with email",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            usernameTextField(),
-            emailTextField(),
-            passwordTextField(),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 150,
-              height: 50,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xff00A86B),
-              ),
-              child: Center(
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+        child: Form(
+          key: _globalKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign up with email",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              usernameTextField(),
+              emailTextField(),
+              passwordTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () {
+                  if (_globalKey.currentState.validate()) {
+                    print("validated");
+                  }
+                },
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xff00A86B),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -75,6 +86,11 @@ class _SignUpPageState extends State<SignUpPage> {
         children: [
           Text("Username"),
           TextFormField(
+            validator: (value) {
+              if (value.isEmpty) return "Username can't be empty";
+              // we will also implement
+              return null;
+            },
             decoration: InputDecoration(
               focusedBorder: UnderlineInputBorder(
                 borderSide: BorderSide(
@@ -96,6 +112,12 @@ class _SignUpPageState extends State<SignUpPage> {
         children: [
           Text("Email"),
           TextFormField(
+            validator: (value) {
+              if (value.isEmpty) return "Email can't be empty";
+              if (!value.contains("@")) return "Email invalid";
+              // we will also implement
+              return null;
+            },
             decoration: InputDecoration(
               focusedBorder: UnderlineInputBorder(
                 borderSide: BorderSide(
@@ -117,6 +139,12 @@ class _SignUpPageState extends State<SignUpPage> {
         children: [
           Text("Password"),
           TextFormField(
+            validator: (value) {
+              if (value.isEmpty) return "password can't be empty";
+              if (value.length <= 8) return "paasword length shold have >=8";
+              // we will also implement
+              return null;
+            },
             decoration: InputDecoration(
               helperText: "Password length should have >=8",
               helperStyle: TextStyle(
