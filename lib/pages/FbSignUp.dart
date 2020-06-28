cbe0b0f9f26f03cf1dc84b485a1b444381f97b12
balram0608 <balram0608@gmail.com>
2020-06-28 23:35:44 +0530

    fb page initated

---
+++ lib/pages/FbSignUp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class name extends StatelessWidget {
+  const name({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
