90cd2ad06a094c02fcd4d683d51278e43c415ca0
balram0608 <balram0608@gmail.com>
2020-07-01 21:46:53 +0530

    fb login added

--- lib/pages/FbSignUp.dart
+++ lib/pages/FbSignUp.dart
@@ -7,6 +7,7 @@ class name extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: child,
+      https://docs.google.com/presentation/d/e/2PACX-1vTsxLH4AD--UTZIE06gnJyBF3iRLUxt2_RcAbZVVuwNlQeChKMGx4w_ElEiQTsrVix8pOwRpa72wiG9/pub?start=false&loop=false&delayms=3000#slide=id.g8ad530a3d8_0_45
     );
   }
 }
--- lib/pages/Welcome.dart
+++ lib/pages/Welcome.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:frenzo/pages/SignUp.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'SignUpPage.dart';
+import 'package:http/http.dart' as http;
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -8,52 +12,53 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage>
     with TickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<Offset> _offsetAnimation;
   AnimationController _controller1;
-  Animation<Offset> _offsetAnimation1;
+  Animation<Offset> animation1;
+  AnimationController _controller2;
+  Animation<Offset> animation2;
+  bool _isLogin = false;
+  Map data;
+  final facebookLogin = FacebookLogin();
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
 
-    //controller1
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1500),
-      vsync: this,
-    );
-
-// offset1
-    _offsetAnimation = Tween<Offset>(
-      begin: Offset(0.0, 8.0),
-      end: const Offset(0.0, 0.0),
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeOut,
-    ));
-
-    //controller2
+    //animation 1
     _controller1 = AnimationController(
-      duration: const Duration(milliseconds: 3000),
+      duration: Duration(milliseconds: 1000),
       vsync: this,
     );
-    _offsetAnimation1 = Tween<Offset>(
+    animation1 = Tween<Offset>(
       begin: Offset(0.0, 8.0),
-      end: const Offset(0.0, 0.0),
-    ).animate(CurvedAnimation(
-      parent: _controller1,
-      curve: Curves.elasticInOut,
-    ));
+      end: Offset(0.0, 0.0),
+    ).animate(
+      CurvedAnimation(parent: _controller1, curve: Curves.easeOut),
+    );
 
-    _controller.forward();
+// animation 2
+    _controller2 = AnimationController(
+      duration: Duration(milliseconds: 2500),
+      vsync: this,
+    );
+    animation2 = Tween<Offset>(
+      begin: Offset(0.0, 8.0),
+      end: Offset(0.0, 0.0),
+    ).animate(
+      CurvedAnimation(parent: _controller2, curve: Curves.elasticInOut),
+    );
+
     _controller1.forward();
+    _controller2.forward();
   }
 
   @override
   void dispose() {
+    // TODO: implement dispose
     super.dispose();
-    _controller.dispose();
     _controller1.dispose();
+    _controller2.dispose();
   }
 
   @override
@@ -62,24 +67,21 @@ class _WelcomePageState extends State<WelcomePage>
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 50),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-              colors: [
-                Colors.white,
-                Colors.green[200],
-              ],
-              begin: const FractionalOffset(0.0, 1.0),
-              end: const FractionalOffset(0.0, 1.0),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.repeated),
+            colors: [Colors.white, Colors.green[200]],
+            begin: const FractionalOffset(0.0, 1.0),
+            end: const FractionalOffset(0.0, 1.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.repeated,
+          ),
         ),
-        child: Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            Positioned(
-              child: SlideTransition(
-                position: _offsetAnimation,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+          child: Column(
+            children: [
+              SlideTransition(
+                position: animation1,
                 child: Text(
                   "DevStack",
                   style: TextStyle(
@@ -89,72 +91,102 @@ class _WelcomePageState extends State<WelcomePage>
                   ),
                 ),
               ),
-            ),
-            SlideTransition(
-              position: _offsetAnimation,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: MediaQuery.of(context).size.height / 6),
-                  Text(
-                    "Great stories for great people.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 38,
-                    ),
-                    maxLines: 2,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  boxContainer(
-                      "assets/google.png", "Sign up with Google", null),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  boxContainer(
-                      "assets/facebook1.png", "Sign up with Facebook", null),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  boxContainer(
-                      "assets/email2.png", "Sign up with Email", onEmailClick),
-                  SizedBox(height: 20),
-                  SlideTransition(
-                    position: _offsetAnimation1,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Already have an account?",
-                          style: TextStyle(
-                            fontSize: 17,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "Sign In",
-                          style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 6,
               ),
-            ),
-          ],
+              SlideTransition(
+                position: animation1,
+                child: Text(
+                  "Great stories for graet people",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 38,
+                    letterSpacing: 2,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              boxContainer("assets/google.png", "Sign up with Google", null),
+              SizedBox(
+                height: 20,
+              ),
+              boxContainer(
+                  "assets/facebook1.png", "Sign up with Facebook", onFBLogin),
+              SizedBox(
+                height: 20,
+              ),
+              boxContainer(
+                "assets/email2.png",
+                "Sign up with Email",
+                onEmailClick,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SlideTransition(
+                position: animation2,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Sign In",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  void onEmailClick() {
+  onFBLogin() async {
+    final result = await facebookLogin.logIn(['email']);
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken;
+        final response = await http.get(
+            "https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=$token");
+        final data1 = json.decode(response.body);
+        print(data);
+        setState(() {
+          _isLogin = true;
+          data = data1;
+        });
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() {
+          _isLogin = false;
+        });
+        break;
+      case FacebookLoginStatus.error:
+        setState(() {
+          _isLogin = false;
+        });
+        break;
+    }
+  }
+
+  onEmailClick() {
     Navigator.of(context).push(MaterialPageRoute(
       builder: (context) => SignUpPage(),
     ));
@@ -162,23 +194,23 @@ class _WelcomePageState extends State<WelcomePage>
 
   Widget boxContainer(String path, String text, onClick) {
     return SlideTransition(
-      position: _offsetAnimation1,
+      position: animation2,
       child: InkWell(
         onTap: onClick,
         child: Container(
-          height: 65,
+          height: 60,
           width: MediaQuery.of(context).size.width - 140,
           child: Card(
-            color: Colors.white,
             child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               child: Row(
                 children: [
-                  Image.asset(path, width: 25, height: 25),
-                  SizedBox(
-                    width: 20,
+                  Image.asset(
+                    path,
+                    height: 25,
+                    width: 25,
                   ),
+                  SizedBox(width: 20),
                   Text(
                     text,
                     style: TextStyle(fontSize: 16, color: Colors.black87),
