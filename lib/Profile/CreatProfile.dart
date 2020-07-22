4ec3be181ea13362791ef51383440259d2d6b3ad
Balram Rathore <balram0698@gmail.com>
2020-07-22 20:18:06 +0530

    creat profile partially completed

--- lib/Profile/CreatProfile.dart
+++ lib/Profile/CreatProfile.dart
@@ -15,26 +15,140 @@ class _CreatProfileState extends State<CreatProfile> {
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: ListView(
           children: <Widget>[
-            inputField(),
-            inputField(),
-            inputField(),
-            inputField(),
+            nameTextField(),
+            SizedBox(
+              height: 20,
+            ),
+            professionTextField(),
+            SizedBox(
+              height: 20,
+            ),
+            dobField(),
+            SizedBox(
+              height: 20,
+            ),
+            titleTextField(),
+            SizedBox(
+              height: 20,
+            ),
+            aboutTextField(),
+            SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget inputField() {
-    return TextField(
-      decoration: new InputDecoration(
+  Widget nameTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.teal,
+        )),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.lightGreen, width: 2.0),
+            borderSide: BorderSide(
+          color: Colors.orange,
+          width: 2,
+        )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.orange, width: 1.0),
+        labelText: "Name",
+        helperText: "Name can't be empty",
+        hintText: "Dev Stack",
+      ),
+    );
+  }
+
+  Widget professionTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.teal,
+        )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.orange,
+          width: 2,
+        )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
         ),
-        hintText: 'Mobile Number',
+        labelText: "Profession",
+        helperText: "Profession can't be empty",
+        hintText: "Full Stack Developer",
+      ),
+    );
+  }
+
+  Widget dobField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.teal,
+        )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.orange,
+          width: 2,
+        )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
+        ),
+        labelText: "Date Of Birth",
+        helperText: "Provide DOB on dd/mm/yyyy",
+        hintText: "01/01/2020",
+      ),
+    );
+  }
+
+  Widget titleTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.teal,
+        )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.orange,
+          width: 2,
+        )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
+        ),
+        labelText: "Title",
+        helperText: "It can't be empty",
+        hintText: "Flutter Developer",
+      ),
+    );
+  }
+
+  Widget aboutTextField() {
+    return TextFormField(
+      maxLines: 4,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.teal,
+        )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+          color: Colors.orange,
+          width: 2,
+        )),
+        labelText: "About",
+        helperText: "Write about yourself",
+        hintText: "I am Dev Stack",
       ),
     );
   }
