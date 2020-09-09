d1e5b0864d12c62c27feff8aa850ef27280f07c6
Balram Rathore <balram0698@gmail.com>
2020-09-09 11:43:09 +0530

    Add Blog Page Added

--- lib/Blog/addBlog.dart
+++ lib/Blog/addBlog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
 class AddBlog extends StatefulWidget {
   AddBlog({Key key}) : super(key: key);
@@ -9,108 +8,108 @@ class AddBlog extends StatefulWidget {
 }
 
 class _AddBlogState extends State<AddBlog> {
-  PickedFile _imageFile;
-  final ImagePicker _picker = ImagePicker();
-  IconData icon = Icons.image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
         backgroundColor: Colors.white54,
+        elevation: 0,
         leading: IconButton(icon: Icon(Icons.clear), onPressed: null),
         actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FlatButton(
-                onPressed: null,
-                child: Text(
-                  "Submit",
-                  style: TextStyle(color: Colors.teal, fontSize: 20),
-                )),
-          )
+          FlatButton(
+              onPressed: null,
+              child: Text(
+                "Preview",
+                style: TextStyle(fontSize: 18, color: Colors.blue),
+              ))
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            textField(),
-            textField2(),
-          ],
-        ),
+      body: ListView(
+        children: <Widget>[
+          titleTextField(),
+          bodyTextField(),
+          SizedBox(
+            height: 20,
+          ),
+          addButton(),
+        ],
       ),
     );
   }
 
-  Widget textField() {
+  Widget titleTextField() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 10,
+      ),
       child: TextFormField(
-        // controller: _name,
-        validator: (value) {
-          if (value.isEmpty) return "Name can't be empty";
-
-          return null;
-        },
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            ),
+          ),
+          labelText: "Add Image and Title",
+          prefixIcon: IconButton(
+            icon: Icon(
+              Icons.image,
+              color: Colors.teal,
+            ),
+            onPressed: null,
+          ),
+        ),
         maxLength: 100,
         maxLines: null,
-        decoration: InputDecoration(
-          prefixIcon: IconButton(
-              icon: Icon(
-                icon,
-                color: Colors.green,
-              ),
-              onPressed: takePhoto),
-          border: OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.teal,
-          )),
-          focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )),
-          labelText: "Cover Image and Title",
-        ),
       ),
     );
   }
 
-  Widget textField2() {
+  Widget bodyTextField() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
       child: TextFormField(
-        // controller: _name,
-        validator: (value) {
-          if (value.isEmpty) return "Name can't be empty";
-
-          return null;
-        },
-        // maxLength: 100,
-        maxLines: null,
         decoration: InputDecoration(
           border: OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.teal,
-          )),
+            borderSide: BorderSide(
+              color: Colors.teal,
+            ),
+          ),
           focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-            color: Colors.orange,
-            width: 2,
-          )),
-          labelText: "body",
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            ),
+          ),
+          labelText: "Provide Body Your Blog",
         ),
+        maxLines: null,
       ),
     );
   }
 
-  void takePhoto() async {
-    final pickedFile = await _picker.getImage(
-      source: ImageSource.gallery,
+  Widget addButton() {
+    return Center(
+      child: Container(
+        height: 50,
+        width: 200,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: Colors.teal),
+        child: Center(
+            child: Text(
+          "Add Blog",
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        )),
+      ),
     );
-    setState(() {
-      _imageFile = pickedFile;
-      icon = Icons.check_box;
-    });
   }
 }
